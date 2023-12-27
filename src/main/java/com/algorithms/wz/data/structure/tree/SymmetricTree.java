f2a186d696515658f9a04737daa6f62a5c9d571a
wangzhi01 <wangzhi01@huohua.cn>
2023-12-27 14:22:45 +0800

    feat：101. 对称二叉树

--- src/main/java/com/algorithms/wz/data/structure/tree/SymmetricTree.java
+++ src/main/java/com/algorithms/wz/data/structure/tree/SymmetricTree.java
@@ -1,5 +1,7 @@
 package com.algorithms.wz.data.structure.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * <a href="https://leetcode-cn.com/problems/symmetric-tree/">101. 对称二叉树</a>
@@ -41,7 +43,35 @@ public class SymmetricTree {
         root1.right = null;
         root2.left = root4;
         SymmetricTree symmetricTree = new SymmetricTree();
-        System.out.println(symmetricTree.isSymmetric(root));
+        System.out.println(symmetricTree.isSymmetric2(root));
+    }
+
+    /**
+     * 队列试试，一次要出两个节点，相同的代码，使用 LinkedList 无错，使用 ArrayDeque 就会出错，这个是因为 ArrayDeque 不支持 null。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+            if (left == null && right == null) {
+                continue;
+            }
+            if ((left == null || right == null) || left.val != right.val) {
+                return false;
+            }
+            queue.offer(left.left);
+            queue.offer(right.right);
+
+            queue.offer(left.right);
+            queue.offer(right.left);
+        }
+        return true;
     }
 
 }
